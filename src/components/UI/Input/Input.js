4022ccbefa6f40d4d7bf8f0b5709a25809900cbb
vsheva <valera.sheva@gmail.com>
2022-07-01 19:40:33 +0300

    refactroring

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -7,13 +7,46 @@ const Input = React.forwardRef((props, ref) => {
 
   const activate = () => {
     inputRef.current.focus();
-  }; //* будет вызвана снаружи Input
+  };
 
-  useImperativeHandle =
-    (ref,
-    () => {
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate,
+    };
+  });
+
+  return (
+    <div className={`${classes.control} ${props.isValid === false ? classes.invalid : ''}`}>
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
+export default Input;
+
+/*old version
+import React, { useRef, useImperativeHandle } from 'react';
+
+import classes from './Input.module.css';
+
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle = (ref, () => {
       return {
-        focus: activate, //external name: internal activate()
+        focus: activate,
       };
     });
 
@@ -36,3 +69,4 @@ const Input = React.forwardRef((props, ref) => {
 });
 
 export default Input;
+*/
