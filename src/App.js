08612b0ff15dbe17db9e8ea69c6aad2ba28c7cb6
vsheva <valera.sheva@gmail.com>
2022-07-01 12:45:30 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, {useContext} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,19 +6,43 @@ import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './store/auth-context';
 
 function App() {
-  const ctx = useContext(AuthContext);
+    const ctx = useContext(AuthContext)
 
-  return (
-    <React.Fragment>
-      <MainHeader />
-      <main>
-        {!ctx.isLoggedIn && <Login />}
-        {ctx.isLoggedIn && <Home />}
-      </main>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <MainHeader/>
+            <main>
+                {!ctx.isLoggedIn && <Login/>}
+                {ctx.isLoggedIn && <Home/>}
+            </main>
+        </React.Fragment>
+
+    );
 }
 
 export default App;
+
+
+//we moved these to AutchContextProvider component
+/**const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+ useEffect(() => {
+        const storedInformation = localStorage.getItem('isLoggedIn');
+        if (storedInformation === "1") {
+            setIsLoggedIn(true);
+        }
+    }, [])
+
+ const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem("isLoggedIn", "1")
+        setIsLoggedIn(true);
+    };
+
+ const logoutHandler = () => {
+        setIsLoggedIn(false);
+    };*/
+
 
 //onLogout={logoutHandler}
